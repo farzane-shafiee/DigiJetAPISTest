cb0fcfdecd00215bc2b7f23df302a9c0d6588a48
farzaneh <lilylotus68@gmail.com>
2023-02-20 22:24:11 +0330

    add api_add_cart_simple, api_add_cart_amazing, api_products, api_shop APIs

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,12 +1,13 @@
+import json
+
 import pytest
 import yaml
 import requests
 
-
 BASE_URL = "https://demo-dknow-api.digikala.com"
 
-class TestBaseConfigDriver:
 
+class TestBaseConfigDriver:
     base_url = ""
 
     def setup_method(self):
@@ -38,9 +39,9 @@ def api_login_register(read_yaml_file):
 def api_confirm_phone(api_login_register):
     path = 'https://demo-dknow-api.digikala.com/user/confirm-phone/'
     payload = dict(
-        user_id=f"{api_login_register.json()['data']['user_id']}",
         token=f"{api_login_register.json()['data']['token']}",
-        code=111111
+        code=111111,
+        phone=f"{api_login_register.json()['data']['phone']}",
     )
     response = requests.post(path, payload)
     return response
@@ -57,3 +58,76 @@ def api_set_address(api_confirm_phone, read_yaml_file):
     return response
 
 
+@pytest.fixture()
+def api_shipping_fee_plan(read_yaml_file):
+    path = "https://demo-dknow-api.digikala.com/admin/shipping/shipping-fee-plan/item/0/?_back=http://demo-dknow-api" \
+           ".digikala.com/admin/shipping/shipping-fee-plan/"
+    payload = dict(
+        free_shipping_threshold=read_yaml_file['free_shipping_threshold']
+    )
+    response = requests.post(path, payload)
+    return response
+
+
+@pytest.fixture()
+def api_shipping_fee_shop_and_cart_close_limit(read_yaml_file):
+    path = "https://demo-dknow-api.digikala.com/admin/shop/item/49/?_back=http://demo-dknow-api.digikala.com/admin" \
+           "/shop/?city%3D%26city_id%3D0%26crud_tab_id%3D%26district_id%3D0%26hash_id%3D%26id%3D%26merchant_id%255B0" \
+           "%255D%3D2%26name%3D%25D8%25B3%25D8%25A8%25D9%2584%25D8%25A7%25D9%2586%26nickname%3D%26radius%3D%26status%3D"
+    payload = dict(
+        shipping_fee_plan_id=read_yaml_file['shipping_fee_plan_id'],
+        cart_close_limit=read_yaml_file['cart_close_limit']
+    )
+    response = requests.post(path, payload)
+    return response
+
+
+@pytest.fixture()
+def api_shop(read_yaml_file, api_confirm_phone):
+    final_token = api_confirm_phone.json()['data']['token']
+    path = '/shop/'f"{read_yaml_file['shop_id']}"'/'
+    headers = {
+        "Authorization": f"{final_token}"
+    }
+    response = requests.get(BASE_URL + path, headers=headers)
+    return response
+
+
+@pytest.fixture()
+def api_products(read_yaml_file):
+    path = '/shop/'f"{read_yaml_file['shop_id']}"'/10/products/'
+    response = requests.get(BASE_URL + path)
+    return response
+
+
+@pytest.fixture()
+def api_add_cart_amazing(api_shop):
+    path = "/cart/add/"
+    # shop_product_id = None
+    # for k, v in api_shop.json()['data']["body"].items():
+    #     for index in v:
+    #         print(f"id = {index['data']['products'][0]['id']}")
+
+    # shop_product_id = api_shop.json()['data']['body']['widgets'][0]['data']['products'][0]['id']
+    # print(shop_product_id)
+    shop_id = api_shop.json()['data']['body']['widgets'][0]['data']['products'][0]['id']
+    payload = {
+        "shop_product_id": shop_id,
+        "source": "web"
+    }
+
+    response = requests.post(BASE_URL + path, payload)
+    print(response.json())
+    return response
+
+
+@pytest.fixture()
+def api_add_cart_simple(api_shop):
+    path = "/cart/add/"
+    # shop_product_id = api_shop.json()['data']['body']['widgets[2]']['data']['products[0]']['id']
+    payload = dict(
+        shop_product_id=f"{api_shop.json()['data']['body']['widgets[2]']['data']['products[0]']['id']}",
+        source="web"
+    )
+    response = requests.post(BASE_URL + path, payload)
+    return response
--- src/tests/test_order/test_orders.py
+++ src/tests/test_order/test_orders.py
@@ -8,3 +8,30 @@ class TestOrder(TestBaseConfigDriver):
         assert api_set_address.status_code == 200
         assert api_set_address.json()['data']['address']['id'] != ""
         assert api_set_address.json()['data']['address']['address'] != ""
+
+    def test_should_set_shipping_fee_plan(self, api_shipping_fee_plan):
+        assert api_shipping_fee_plan.status_code == 200
+
+    def test_should_set_shipping_fee_shop_and_cart_close_limit(self, api_shipping_fee_shop_and_cart_close_limit):
+        assert api_shipping_fee_shop_and_cart_close_limit.status_code == 200
+
+    def test_should_show_shop(self, api_shop):
+        assert api_shop.status_code == 200
+        assert api_shop.json()['data']['header']['shop']['id'] != ""
+        assert api_shop.json()['data']['header']['shop']['status'] == "active"
+
+    def test_should_show_products(self, api_products):
+        assert api_products.status_code == 200
+        assert api_products.json()['data']['products'] != ""
+
+    def test_should_add_cart_amazing(self, api_add_cart_amazing):
+        assert api_add_cart_amazing.status_code == 200
+        # assert api_add_cart_amazing.json()['data']['cart_shipment']['hash_id'] != ""
+        # assert api_add_cart_amazing.json()['data']['cart_shipment']['cart_items']['items[0]']['item_id'] != ""
+        # assert api_add_cart_amazing.json()['data']['cart_shipment']['cart_items']['items[0]']['product']['id'] == api_add_cart_amazing.shop_product_id
+
+    def test_should_add_cart_simple(self, api_add_cart_simple, shop_product_id):
+        assert api_add_cart_simple.status_code == 200
+        assert api_add_cart_simple.json()['data']['cart_shipment']['hash_id'] != ""
+        assert api_add_cart_simple.json()['data']['cart_shipment']['cart_items']['items[0]']['item_id'] != ""
+        assert api_add_cart_simple.json()['data']['cart_shipment']['cart_items']['items[0]']['product']['id'] == shop_product_id
